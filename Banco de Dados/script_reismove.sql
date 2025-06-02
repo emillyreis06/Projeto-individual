db6fbc39374c72fbe096df861fde14254367ef08
emillyreis06 <emilly.silva@sptech.school>
2025-06-02 19:31:36 -0300

    atualizando modelagem e script

--- Banco de Dados/script_reismove.sql
+++ Banco de Dados/script_reismove.sql
@@ -1,6 +1,7 @@
 create database reismove;
 use reismove;
 
+
 create table usuario (
 idUsuario int primary key auto_increment,
 nome varchar (45),
@@ -10,14 +11,13 @@ confirmarSenha varchar (15)
 );
 
 
-
 create table formulario (
 idFormulario int primary key auto_increment,
 faixa_etaria char (10),
 constraint chkEtaria check (faixa_etaria in ("15-20","20-25","25-30","30-35", "40+")),
 genero char (15),
 constraint chkGenero check (genero in ("Feminino","Masculino")),
-pratica_ativFisica varchar (6),
+pratica_ativFisica char (6),
 constraint chkPratica check (pratica_ativFisica in ("Sim","Não")),
 qual_ativFisica varchar (20),
 constraint chkQual check (qual_ativFisica in ("Musculação","Crossfit","Esporte","Outro")),
@@ -26,7 +26,7 @@ constraint chkFreq  check ( frequencia in ("1 a 3 vezes por semana", "4 a 6 veze
 treino_preferido varchar (45),
 constraint  chkTreino check (treino_preferido in ("Treino de posterior", "Treino de quadríceps", "Treino de costas","Treino de bíceps/tríceps", "Treino de ombro")),
 obj varchar (45),
-constraint chkObj check (obj in ("Ganhar massa magra", "Emagrecer", "Bem estar físico e mental", "Saúde", "Estética", "Outro")),
+constraint chkObj check (obj in ("Ganhar massa magra", "Emagrecer", "Bem estar físico e mental", "Saúde")),
 fkUsuario int,
 constraint fkFormUser foreign key (fkUsuario) references usuario (idUsuario)
 );
@@ -36,5 +36,36 @@ constraint fkFormUser foreign key (fkUsuario) references usuario (idUsuario)
 select * from usuario;
 select * from formulario;
 
-insert 
+-- conta quantas pessoas respodeu o formulario
+SELECT count(*) as 'Total de Respostas' from formulario;
+
+-- quantas pessoas praticam atividade fisica
+select count(*) from formulario where  pratica_ativFisica = 'Sim';
+
+
+-- qual o obejtivo mais selecionado pelas pessoas 
+SELECT obj, COUNT(*) AS quantidade
+FROM formulario
+GROUP BY obj
+HAVING COUNT(*) = (
+    SELECT MAX(contagem) FROM (
+        SELECT COUNT(*) AS contagem
+        FROM formulario
+        GROUP BY obj
+    ) AS contagens
+);
+
+
+-- exibir quantidade de pessoas por genero
+SELECT genero, count(*) AS 'quantidade'
+FROM formulario
+WHERE pratica_ativFisica = 'Sim'
+GROUP BY genero;
+
+-- obter  a faixa etária
+SELECT faixa_etaria, COUNT(*) as 'Faixa Etária'
+FROM formulario
+WHERE pratica_ativFisica = 'Sim'
+GROUP BY faixa_etaria
+order by faixa_etaria;
 
